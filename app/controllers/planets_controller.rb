f0b8b7650db7036edd591e5a5e1781eac099f750
amanda vikdal <vikdal.amanda@gmail.com>
2023-10-16 11:11:35 -0400

    commit tree 6172932abe8327c57d7cfbd9507942931b46d57a

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -1,10 +1,33 @@
 class PlanetsController < ApplicationController
-  before_action :set_planet, only: %i[ show update destroy ]
+  # before_action :set_planet, only: %i[ show update destroy ]
 
   # GET /planets
   def index
-    planets = Planet.all
-
-    render json: planets
+    render json: Planet.all
   end
+
+  #SHOW /planets/:id
+  def show
+    planet = Planet.find(params[:id])
+    render json: planet, status: :ok
+  end
+
+  def create
+    planet = Planet.create!(planet_params)
+  end
+
+  def update
+    byebug
+  end
+
+  def destroy
+    byebug
+  end
+
+  private
+
+  def planet_params
+    params.permit(:name, :distance_from_earth)
+  end
+
 end
